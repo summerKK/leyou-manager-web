95077138467391875228c28c5945b5d2f56a1dab
Summer <su943515688@gmail.com>
2019-07-15 17:44:44 +0800

    [*] 从后端获取品牌数据

--- src/pages/item/Brand.vue
+++ src/pages/item/Brand.vue
@@ -6,7 +6,7 @@
       </v-flex>
       <v-spacer/>
       <v-flex xs5>
-        <v-text-field label="品牌搜索" append-icon="search" hide-details="true" v-model="name"/>
+        <v-text-field label="品牌搜索" append-icon="search" :hide-details="true" v-model="name"/>
       </v-flex>
     </v-layout>
     <v-data-table
@@ -55,43 +55,12 @@
       }
     },
     created() {
-      this.brands = [
-        {
-          "id": 2032,
-          "name": "OPPO",
-          "image": "http://img10.360buyimg.com/popshop/jfs/t2119/133/2264148064/4303/b8ab3755/56b2f385N8e4eb051.jpg",
-          "letter": "O"
-        },
-        {
-          "id": 2033,
-          "name": "飞利浦（PHILIPS）",
-          "image": "http://img12.360buyimg.com/popshop/jfs/t18361/122/1318410299/1870/36fe70c9/5ac43a4dNa44a0ce0.jpg",
-          "letter": "F"
-        },
-        {
-          "id": 2034,
-          "name": "华为（HUAWEI）",
-          "image": "http://img10.360buyimg.com/popshop/jfs/t5662/36/8888655583/7806/1c629c01/598033b4Nd6055897.jpg",
-          "letter": "H"
-        },
-        {
-          "id": 2036,
-          "name": "酷派（Coolpad）",
-          "image": "http://img10.360buyimg.com/popshop/jfs/t2521/347/883897149/3732/91c917ec/5670cf96Ncffa2ae6.jpg",
-          "letter": "K"
-        },
-        {
-          "id": 2037,
-          "name": "魅族（MEIZU）",
-          "image": "http://img13.360buyimg.com/popshop/jfs/t3511/131/31887105/4943/48f83fa9/57fdf4b8N6e95624d.jpg",
-          "letter": "M"
-        }
-      ]
-      this.totalBrands = 50
+      this.loadBrands()
     },
+
     methods: {
       loadBrands() {
-        this.$http.get('/brand/list', {
+        this.$http.get('/item/brand/list', {
           params: {
             name: this.name,
             desc: this.pagination.descending,
@@ -99,9 +68,15 @@
             rowsPerPage: this.pagination.rowsPerPage,
             sortBy: this.pagination.sortBy,
           }
+        }).then(resp => {
+          this.brands = resp.data.items
+          this.totalBrands = resp.data.total
+          // 完成赋值后，把加载状态赋值为false
+          this.loading = false
         })
       }
     },
+
     watch: {
       name: {
         handler() {
